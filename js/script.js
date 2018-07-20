8f2dc06d5e2e395d45e589d6155e48f809ce1d5d
Winkii <plushka002@gmail.com>
2018-07-20 22:11:59 +0300

    make final corrs

--- js/script.js
+++ js/script.js
@@ -1,29 +1,44 @@
 'use strict';
 
+var body = document.querySelector('body');
 var mainTable = document.querySelector('.main-table');
 var inputs = Array.from(mainTable.querySelectorAll('input[type="text"]'));
+var selectedElements = mainTable.querySelector('.first-row__span');
+var itemCheckbox = Array.from(mainTable.querySelectorAll('input[type=checkbox]'));
+var removeSelected = mainTable.querySelector('.remove_checked-elements');
 var popup = document.querySelector('.popup');
 var pricePopup = popup.querySelector('.popup__price');
 var inputPopup = popup.querySelector('#quantity__item');
 var valuePopup = popup.querySelector('.popup__value');
-var submitButton = document.querySelector('.popup__submit');
-var resetButton = document.querySelector('.popup__reset');
-var selectedElements = document.querySelector('.first-row__span');
-var itemCheckbox = Array.from(document.querySelectorAll('input[type=checkbox]'));
-var removeSelected = document.querySelector('.remove_checked-elements');
-var changeInputButton = document.querySelectorAll('.input__counter');
+var submitButton = popup.querySelector('.popup__submit');
+var resetButton = popup.querySelector('.popup__reset');
+var changeInputButton = popup.querySelectorAll('.input__counter');
 
 function inputHandler(event) {
-
   var input = event.target || event.srcElement;
   var id = input.id;
+  var coords = input.getBoundingClientRect();
+
+  popup.style.left = coords.left - 90 + "px";
+  popup.style.top = coords.bottom + 5 + "px";
 
   popup.classList.remove('popup_hidden');
+  inputPopup.focus();
   inputPopup.value = input.value;
   inputPopup.setAttribute('item_id', id);
 
   setPopupPrice(id);
-  setPopupValue()
+  setPopupValue();
+  addPopupHandler(event);
+
+  body.addEventListener('click', closePopup);
+}
+
+function closePopup(event) {
+  if (event.target.className === 'input-quantity') return;
+  if(!event.path.includes(document.querySelector('.popup'))) {
+    popup.classList.add('popup_hidden');
+  }
 }
 
 function setPopupPrice(id) {
@@ -59,7 +74,6 @@ function setSelectedItems() {
   selectedElements.textContent = String(checked);
 }
 
-
 function validatePopup() {
   var value = Number(inputPopup.value);
   var min = Number(inputPopup.getAttribute('min'));
@@ -88,7 +102,24 @@ inputs.forEach(function (input) {
   input.addEventListener("click", inputHandler);
 });
 
-inputPopup.addEventListener('keyup', validatePopup);
+function changeInputPopup(event) {
+  inputPopup.value = Number(inputPopup.value) + Number(event.target.textContent + 1);
+  validatePopup();
+}
+
+function addPopupHandler() {
+  changeInputButton.forEach(function (button) {
+    button.addEventListener('click', changeInputPopup);
+  });
+  inputPopup.addEventListener('keyup', validatePopup);
+}
+
+function deleteButtonHandler() {
+  changeInputButton.forEach(function (button) {
+    button.removeEventListener('click', changeInputPopup);
+  });
+  inputPopup.removeEventListener('keyup', validatePopup);
+}
 
 submitButton.addEventListener('click', function () {
   var item_id = inputPopup.getAttribute('item_id');
@@ -98,22 +129,18 @@ submitButton.addEventListener('click', function () {
   item_input.value = inputPopup.value;
   item_value.textContent = valuePopup.textContent;
   popup.classList.add('popup_hidden');
+
+  deleteButtonHandler();
   summaryValue();
 });
 
 resetButton.addEventListener('click', function () {
-  popup.classList.add('popup_hidden');
+  deleteButtonHandler();
+  popup.classList.add('popup_hidden')
 });
 
 itemCheckbox.forEach(function (checkbox) {
   checkbox.addEventListener('change', setSelectedItems);
-});
-
-changeInputButton.forEach(function (button) {
-  button.addEventListener('click', function (event) {
-    inputPopup.value = Number(inputPopup.value) + Number(event.target.textContent + 1);
-    validatePopup();
-  });
 });
 
 removeSelected.addEventListener('click', function () {
@@ -121,8 +148,7 @@ removeSelected.addEventListener('click', function () {
   checked.forEach(function (checkbox) {
     checkbox.parentElement.parentElement.parentElement.remove()
   });
+
   setSelectedItems();
+  summaryValue();
 });
-
-
-
